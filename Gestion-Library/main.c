fde682f7093d627f58843372bf429b57a23ba718
PlugNCS <michael.nass@free.fr>
2020-05-05 18:11:29 +0200

    Initial Commit

---
+++ Gestion-Library/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Gestion-Library
+//
+//  Created by PlugN on 05/05/2020.
+//  Copyright © 2020 PlugN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
